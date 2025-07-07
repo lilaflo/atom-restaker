5ab86856ad1974432ed29bb6fcf4ead30c78bb5e
Florian Fackler <gitea@lale.li>
2025-07-07 10:59:08 +0200

    Remove test script

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { sendDiscordNotification } from "./notify";
-
-async function main(): Promise<void> {
-  const message = "This is a re-stake test";
-  await sendDiscordNotification(message);
-}
-
-main();
